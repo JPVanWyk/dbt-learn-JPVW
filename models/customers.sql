11d7fbcf859c53c1a2bde8835ac864b4778eb10c
JP Van Wyk <jean-paul.vanwyk@usaa.com>
2020-10-30 20:09:49 +0000

    Test

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,29 @@
+{{
+  config(
+    materialized='view'
+  )
+}}
+
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+   select
+    id as customer_id,
+    first_name,
+    last_name
 
-    from raw.jaffle_shop.customers
+from raw.jaffle_shop.customers
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
+  select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
 
-    from raw.jaffle_shop.orders
+from raw.jaffle_shop.orders
 
 ),
 
